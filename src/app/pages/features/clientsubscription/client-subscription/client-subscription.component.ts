52bc614fadf15aa9e2e379f83b342b60ce3d1b25
mohamed <mohamed.kassem@gmail.com>
2024-10-22 23:58:16 +0300

    form-ui design

--- src/app/pages/features/clientsubscription/client-subscription/client-subscription.component.ts
+++ src/app/pages/features/clientsubscription/client-subscription/client-subscription.component.ts
@@ -7,43 +7,47 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-client-subscription',
   templateUrl: './client-subscription.component.html',
-  styleUrl: './client-subscription.component.css'
+  styleUrl: './client-subscription.component.css',
 })
-export class ClientSubscriptionComponent implements OnInit{
+export class ClientSubscriptionComponent implements OnInit {
   addForm!: FormGroup;
 
-  constructor(private fb: FormBuilder, private clientSubscriptService: ClientSubscriptionService,private router: Router) {}
+  constructor(
+    private fb: FormBuilder,
+    private clientSubscriptService: ClientSubscriptionService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.addForm = this.fb.group({
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
-      phoneNumber: ['', Validators.required]
+      phoneNumber: ['', Validators.required],
     });
   }
 
   onSubmit() {
     if (this.addForm.valid) {
-      this.clientSubscriptService.AddConferenceSubscription(this.addForm.value).subscribe(
-        response => {
-          if(response.status_code==200){
-            console.log('User added successfully!', response);
-            this.router.navigate(['/success']);}
-          else
-            this.router.navigate(['/error']);
+      this.clientSubscriptService
+        .AddConferenceSubscription(this.addForm.value)
+        .subscribe(
+          (response) => {
+            if (response.status_code == 200) {
+              console.log('User added successfully!', response);
+              this.router.navigate(['/success']);
+            } else this.router.navigate(['/error']);
           },
-        error => {
-          console.error('Error adding user:', error);
-          this.router.navigate(['/error']);
-        }
-      );
-    }
-    else
-    this.checkFormErrors(); 
+          (error) => {
+            console.error('Error adding user:', error);
+            this.router.navigate(['/error']);
+          }
+        );
+    } else this.checkFormErrors();
   }
+
   checkFormErrors() {
-    Object.keys(this.addForm.controls).forEach(key => {
+    Object.keys(this.addForm.controls).forEach((key) => {
       const controlErrors = this.addForm.get(key)?.errors;
       if (controlErrors) {
         console.log(`Error in ${key}:`, controlErrors);
