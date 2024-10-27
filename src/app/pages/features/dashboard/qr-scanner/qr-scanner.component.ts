7a6764208780d36fb30fb1ca8c5c0a6277febf87
AhmedTaha-tech <ahmed.taha2027@gmail.com>
2024-10-27 12:19:48 +0300

    Revert

--- src/app/pages/features/dashboard/qr-scanner/qr-scanner.component.ts
+++ src/app/pages/features/dashboard/qr-scanner/qr-scanner.component.ts
@@ -1,47 +1,110 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { QrService } from '../../../../services/qr.service';
 import { BrowserMultiFormatReader } from '@zxing/browser';
-import { NgxScannerQrcodeComponent, NgxScannerQrcodeService, ScannerQRCodeConfig, ScannerQRCodeResult, ScannerQRCodeSelectedFiles} from 'ngx-scanner-qrcode';
+import { NgxScannerQrcodeComponent, NgxScannerQrcodeService, ScannerQRCodeConfig, ScannerQRCodeResult, ScannerQRCodeSelectedFiles } from 'ngx-scanner-qrcode';
 
 @Component({
   selector: 'app-qr-scanner',
   templateUrl: './qr-scanner.component.html',
   styleUrl: './qr-scanner.component.css'
 })
-export class QrScannerComponent{
+export class QrScannerComponent implements AfterViewInit {
 
-  config: Object = {
-    isAuto: true,
-    text: { font: '25px serif' }, // Hiden { font: '0px' },
-    frame: { lineWidth: 8 },
-    medias: {
-      audio: false,
-      video: {
-        facingMode: 'environment', // To require the rear camera https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
-        width: { ideal: 1280 },
-        height: { ideal: 720 }
-      }
-    }
+  errorMessage: string | null = null;
+  qrCodeText: string | null = null;
+  showSuccessMessage = false;
+
+  body = {
+    qrCodeTextContent: this.qrCodeText,
   };
 
-  //selectedFiles: SelectedFiles[] = [];
+  // https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia#front_and_back_camera
+  config: ScannerQRCodeConfig = {
+    constraints: {
+      video: {
+        width: window.innerWidth
+      },
+    },
+    // canvasStyles: [
+    //   { /* layer */
+    //     lineWidth: 1,
+    //     fillStyle: '#00950685',
+    //     strokeStyle: '#00950685',
+    //   },
+    //   { /* text */
+    //     font: '17px serif',
+    //     fillStyle: '#ff0000',
+    //     strokeStyle: '#ff0000',
+    //   }
+    // ],
+  };
 
-  constructor(private qrcode: NgxScannerQrcodeService) { }
+  public qrCodeResult: ScannerQRCodeSelectedFiles[] = [];
+  public qrCodeResult2: ScannerQRCodeSelectedFiles[] = [];
 
-   onError(e: any): void {
-    alert(e);
+  @ViewChild('action') action!: NgxScannerQrcodeComponent;
+
+  public percentage = 80;
+  public quality = 100;
+
+  constructor(private qrcode: NgxScannerQrcodeService,private qrService: QrService) { }
+
+  ngAfterViewInit(): void {
+    this.action.isReady.subscribe((res: any) => {
+      this.handle(this.action, 'start');
+    });
   }
 
-  handle(action: any, fn: string): void {
-    action[fn]().subscribe(console.log, console.error);
+  public onEvent(e: ScannerQRCodeResult[], action?: any): void {
+    // e && action && action.pause();
+    console.log(e);
+    this.qrCodeText=e[0].value;
+    this.body.qrCodeTextContent=this.qrCodeText;
   }
 
-  // onSelects(files: any) {
-  //   this.qrcode.loadFiles(files, this.config).subscribe(res => {
-  //     this.selectedFiles = res.filter(f => f.urlQR);
-  //     console.log(res); // v1.0.25 Fixed issue https://stackoverflow.com/questions/74245551/ngx-scanner-qrcode-reading-data-in-ts
-  //   });
-  // }
+  public handle(action: any, fn: string): void {
+    const playDeviceFacingBack = (devices: any[]) => {
+      // front camera or back camera check here!
+      const device = devices.find(f => (/back|rear|environment/gi.test(f.label))); // Default Back Facing Camera
+      action.playDevice(device ? device.deviceId : devices[0].deviceId);
+    }
+
+    if (fn === 'start') {
+      action[fn](playDeviceFacingBack).subscribe((r: any) => console.log(fn, r), alert);
+    } else {
+      action[fn]().subscribe((r: any) => console.log(fn, r), alert);
+    }
+  }
+
+  public onDowload(action: NgxScannerQrcodeComponent) {
+    action.download().subscribe(console.log, alert);
+  }
+
+  public onSelects(files: any) {
+    this.qrcode.loadFiles(files, this.percentage, this.quality).subscribe((res: ScannerQRCodeSelectedFiles[]) => {
+      this.qrCodeResult = res;
+    });
+  }
+
+  public onSelects2(files: any) {
+    this.qrcode.loadFilesToScan(files, this.config, this.percentage, this.quality).subscribe((res: ScannerQRCodeSelectedFiles[]) => {
+      console.log(res);
+      this.qrCodeResult2 = res;
+    });
+  }
+
+  public onGetConstraints() {
+    const constrains = this.action.getConstraints();
+    console.log(constrains);
+  }
+  
+  public applyConstraints() {
+    const constrains = this.action.applyConstraints({
+      ...this.action.getConstraints(),
+      width: 510
+    });
+    console.log(constrains);
+  }
 
 
 
@@ -50,30 +113,29 @@ export class QrScannerComponent{
 
 
 
+  ConfirmSubscriberAttendance() {
+    if (this.qrCodeText != '') {
+      this.qrService.ConfirmSubscriberAttendance(this.body).subscribe(
+        (response) => {
+          if (response.status_code == 200) {
+            console.log('QR Code scanned successfully!', response);
+            this.showSuccessMessage = true; // Show success message
+            this.hideSuccessMessageAfterDelay(); // Call the method to hide it after a delay
+          }
+          else
+          this.errorMessage="Error "
+        },
+        (error) => {
+          console.error('Error in QR Code Scanner :', error);
+        }
+      );
+    }
+  }
 
-  // ConfirmSubscriberAttendance() {
-  //   if (this.qrCodeText != '') {
-  //     this.qrService.ConfirmSubscriberAttendance(this.body).subscribe(
-  //       (response) => {
-  //         if (response.status_code == 200) {
-  //           console.log('QR Code scanned successfully!', response);
-  //           this.showSuccessMessage = true; // Show success message
-  //           this.hideSuccessMessageAfterDelay(); // Call the method to hide it after a delay
-  //         }
-  //         else
-  //         this.errorMessage="Error "
-  //       },
-  //       (error) => {
-  //         console.error('Error in QR Code Scanner :', error);
-  //       }
-  //     );
-  //   }
-  // }
-
-  // hideSuccessMessageAfterDelay() {
-  //   setTimeout(() => {
-  //     this.showSuccessMessage = false; // Hide success message after 5 seconds
-  //     this.qrCodeText=''
-  //   }, 5000); // 5000 milliseconds = 5 seconds
-  // }
+  hideSuccessMessageAfterDelay() {
+    setTimeout(() => {
+      this.showSuccessMessage = false; // Hide success message after 5 seconds
+      this.qrCodeText=''
+    }, 5000); // 5000 milliseconds = 5 seconds
+  }
 }
